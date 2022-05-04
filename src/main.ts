bf94709a8f92c7cfcde09863656d8eec156ecbeb
Arash-11 <arashnawy@gmail.com>
2022-05-03 21:54:26 -0400

    refactored a bunch of stuff and added textarea to paste in text

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { UploadBtn } from "./components/UploadBtn";
-import { URLForm } from "./components/URLForm";
+// import { UploadBtn } from "./components/UploadBtn";
+// import { URLForm } from "./components/URLForm";
 
 const app = document.querySelector<HTMLElement>('[data-main]')!;
 
 // The UploadBtn class instantiation is just for more enhanced accessibility --
 // it only toggles the `aria-expanded` attribute on the button.
 // The options dropdown's reveal/hide is handled in the CSS.
-new UploadBtn();
+// new UploadBtn();
 
-const urlForm = new URLForm();
+// const urlForm = new URLForm();
 
-app.append(urlForm.el);
+// app.append(urlForm.el);
 
 export {};
