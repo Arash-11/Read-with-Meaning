eb3f43b057b9072d6779708224264fe9d763f706
Arash-11 <arashnawy@gmail.com>
2022-07-16 15:59:11 -0400

    Used multiline comment format

--- src/components/Text.ts
+++ src/components/Text.ts
@@ -51,41 +51,49 @@ export default class Text {
 
     if (!textContent?.trim()) return '';
 
-    // Limit `endOffset` to additional 50 boundary points in the range.
-    // This is to avoid grabbing entire large batches of texts unnecessarily, since doing that
-    // will cause performance issues with large amounts of text (eg. a 50-page PDF).
+    /*
+      Limit `endOffset` to additional 50 boundary points in the range, to avoid grabbing
+      entire large batches of texts unnecessarily, as that can cause performance issues
+      with large amounts of text (eg. a 50-page PDF).
+    */
     const endOffset = startOffset + 50;
     let rangeStartText = '';
-    let word = null;
+    let word = '';
 
-    // If start of text (i.e. area right before the first letter of the first word) is clicked,
-    // select the first word in the text.
+    /*
+      If start of text (i.e. area right before the first letter of the first word) is clicked,
+      select the first word in the text.
+    */
     if (startOffset === 0) {
       const textToProcess = textContent.substring(0, endOffset);
       word = textToProcess.match(/(\w+)/g)![0];
     } else {
-      // Iterate backwards from the position (i.e. the letter in a word) that was clicked
-      // until a non-letter is encountered -- eg. a space (which will probably mean the start of a word),
-      // a punctuation mark, words with numbers in them that aren't in the dictionary, etc.
+      /*
+        Iterate backwards from the position (i.e. the letter in a word) that was clicked
+        until a non-letter is encountered -- eg. a space (which will probably mean the start of a word),
+        a punctuation mark, words with numbers in them that aren't in the dictionary, etc.
+      */
       for (let i = startOffset; lettersOnlyRegex.test(textContent.substring(i - 1, i)); i--) {
         rangeStartText = textContent.substring(i - 1, endOffset);
-        // Break out of the loop if you've reached the start of the first word in the text.
-        // This will be used when you click anywhere in the first letter in the text,
-        // except the area right before the first letter.
+        /*
+          Break out of the loop if you've reached the start of the first word in the text.
+          This will be used when you click anywhere in the first letter in the text,
+          except the area right before the first letter.
+        */
         if (i === 0) {
           rangeStartText = textContent.substring(0, endOffset);
           break;
         }
       }
 
-      // If a user highlights a text, set the word to that highlighted text.
-      // Otherwise, it will be the first word in the "cut-off" text that we got after
-      // iterating backwards, as defined in the for-loop above, or an empty string.
-      word = selection.toString()
-        ? selection.toString()
-        : rangeStartText
-        ? rangeStartText.match(/(\w+)/g)![0]
-        : '';
+      /*
+        If a user highlights a text, set the word to that highlighted text.
+        Otherwise, it will be the first word in the "cut-off" text that we got after
+        iterating backwards, as defined in the for-loop above, or it'll remain an empty string.
+      */
+      if (selection.toString()) word = selection.toString();
+      else if (rangeStartText) word = rangeStartText.match(/(\w+)/g)![0];
+      
     }
 
     // Only letters are accepted at the moment (numbers, symbols, etc. are invalid).
@@ -105,9 +113,11 @@ export default class Text {
   }
 
   async handleTextClick(e: MouseEvent | PointerEvent): Promise<void> {
-    // Ignore any additional consecutive additional clicks.
-    // This will help prevent the `getWordDefinition` function from unnecessarily firing more than once
-    // (eg. when a user double clicks to highlight a word).
+    /*
+      Ignore any additional consecutive additional clicks.
+      This will help prevent the `getWordDefinition` function from unnecessarily firing more than once
+      (eg. when a user double clicks to highlight a word).
+    */
     if (e.detail > 1) return;
 
     const { word, definition } = await this.getWordDefinition();
